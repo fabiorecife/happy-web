c8f9d9913ce783513e4e58f267b91a1789403def
Fabio Almeida <fabiorecife@gmail.com>
2020-10-12 18:34:35 -0300

    add OrphanagesMap page and routes

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import mapMarkerImg from '../images/map-marker.svg'
+import {Link} from 'react-router-dom'
+import {FiPlus} from 'react-icons/fi'
+import '../styles/pages/orphanages-map.css'
+
+import 'leaflet/dist/leaflet.css'
+
+import { Map, TileLayer } from 'react-leaflet'
+
+function OrphanagesMap() {
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Marker"/>
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>Recife</strong>
+                    <span>Pernambuco</span>
+                </footer>
+            </aside>
+            <Map
+                center={[-8.0786066,-34.9382238]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </Map>
+            <Link to="" className="create-orphanage" >
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
+    )
+}
+
+export default OrphanagesMap
