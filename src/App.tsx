77ff7c23d7d723e1b0820727b2922a474f6eefb9
Fabio Almeida <fabiorecife@gmail.com>
2020-10-12 14:45:22 -0300

    add landing page content

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import './styles/global.css'
+import './styles/pages/landing.css'
+import logoImg from './images/logo.svg'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello NLW!</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Logo Happy"/>
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia
+            de muitas crianças.</p>
+        </main>
+        <div className="location">
+          <strong>Recife</strong>
+          <span>Pernambuco</span>
+        </div>
+        <a href="/" className="enter-app">
+          >
+        </a>
+      </div>
     </div>
   );
 }
