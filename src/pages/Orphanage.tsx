73bbe1a337fbeca3051c933c1ad0d84410a46ece
Fabio Almeida <fabiorecife@gmail.com>
2020-10-15 07:38:10 -0300

    Dia 3 - #1 - finalizando pagina de mapa

--- src/pages/Orphanage.tsx
+++ src/pages/Orphanage.tsx
@@ -73,13 +73,13 @@ export default function Orphanage() {
                 doubleClickZoom={false}
               >
                 <TileLayer 
-                  url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                  url={`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`}
                 />
                 <Marker interactive={false} icon={happyMapIcon} position={[-27.2092052,-49.6401092]} />
               </Map>
 
               <footer>
-                <a href="">Ver rotas no Google Maps</a>
+                <a href="/">Ver rotas no Google Maps</a>
               </footer>
             </div>
 
